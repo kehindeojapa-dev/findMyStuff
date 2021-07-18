9b5ae46ff6c03ef61005427cb6b305ef0a59b998
Kehinde <adaeza.dev@gmail.com>
2021-07-18 16:32:29 +0100

    finished working on newEntry screen, next work is on the delete screen, after that is the update screen

--- src/Components/NewEntry.js
+++ src/Components/NewEntry.js
@@ -1,14 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Style Import
 import "./styles/NewEntry.css";
 
+const locations = [
+  { id: 1, location: "Attic" },
+  { id: 2, location: "Garage" },
+  { id: 3, location: "Wardrobe" },
+];
+
 const NewEntry = ({ showEntry }) => {
+  const [showNew, setShowNew] = useState(false);
+  const [locationText, setLocationText] = useState("New Location?");
+
+  const showNewLocation = () => {
+    setShowNew(!showNew);
+    if (locationText === "New Location?") {
+      setLocationText("Old Location?");
+    } else {
+      setLocationText("New Location?");
+    }
+  };
+
+  const NewLocation = () => {
+    return (
+      <>
+        <input type="text" name="newlocation" />
+      </>
+    );
+  };
+
   return (
     <>
       <section className="newentry">
-        <button onClick={() => showEntry(false)}>X</button>
-        <h2>Am a new entry</h2>
+        <button onClick={() => showEntry(false)} className="newentry-closebtn">
+          X
+        </button>
+        <h2>Track New Item</h2>
+        <form className="newentry-form">
+          <label htmlFor="item">Item Name</label>
+          <input type="text" name="item" />
+          <div className="newentry-location">
+            <label htmlFor="location">Pick location</label>
+            <input
+              type="button"
+              className="newlocation-btn"
+              value={locationText}
+              onClick={() => showNewLocation()}
+            />
+          </div>
+          {showNew ? (
+            <NewLocation />
+          ) : (
+            <select name="location">
+              <option value="unselected">select a location</option>;
+              {locations.map((storage) => {
+                return (
+                  <option key={storage.id} value={storage.location}>
+                    {storage.location}
+                  </option>
+                );
+              })}
+            </select>
+          )}
+
+          <label htmlFor="description">Describe location</label>
+          <textarea name="description"></textarea>
+          <label htmlFor="priority">Priority</label>
+          <select name="priority">
+            <option value="unselected">select priority</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+          <input type="hidden" value={"daniel@gmail.com"} name="user" />
+          <input type="submit" value="Track" />
+        </form>
       </section>
     </>
   );
